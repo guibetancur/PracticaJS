04993acb6d41fbd3b1c5c7f0f3c15858aaf5d8ef
Guillermo Betancur <guibetancur@gmail.com>
2023-04-28 22:01:24 -0500

    Conteo Regresivo

--- retodom.js
+++ retodom.js
@@ -1,6 +1,7 @@
 import hamburgerMenu from "./index_retodom.js";
 import {digitalClock, alarm} from "./section1.js";
 import {moveBall} from "./section2.js";
+import countdown from "./section3.js";
 
 const d = document
 
@@ -8,6 +9,8 @@ d.addEventListener("DOMContentLoaded", (e) => {
     hamburgerMenu('.panel-btn', '.panel', '.menu a')
     digitalClock('#reloj','#btnReloj')
     alarm('./assets/alarma_despertador_relax.mp3', '#btnAlarma')
+    // countdown('Apr 24, 2024 11:55:00','Felicitaciones')
+    countdown('Apr 28, 2023 22:00:00','El plazo se ha cumplido... Felicitaciones')
 })
 
 d.addEventListener('keydown', e => {
