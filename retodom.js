241ef312bd0ee6757f776c6b1ac0d540abd92bdf
Guillermo Betancur <guibetancur@gmail.com>
2023-05-02 20:34:05 -0500

    Responsive design con Javascript

--- retodom.js
+++ retodom.js
@@ -2,8 +2,9 @@ import hamburgerMenu from "./index_retodom.js";
 import {digitalClock, alarm} from "./section1.js";
 import {moveBall} from "./section2.js";
 import countdown from "./section3.js";
-import botonHome from "./section4.js";
-import tema from "./section5.js";
+import botonHome from "./botonhome.js";
+import tema from "./temas.js";
+import responsiveMedia from "./responsive.js";
 
 const d = document
 
@@ -14,10 +15,14 @@ d.addEventListener("DOMContentLoaded", (e) => {
     countdown('Jul 17, 2023 00:00:00','Felicitaciones')
     // countdown('Apr 28, 2023 22:00:00','El plazo se ha cumplido... Felicitaciones')
     botonHome('.scroll-top-btn')
-    tema('.btnTema','modo')
+    responsiveMedia('youtube','(min-width: 1024px)',`<a href='https://youtu.be/JSwu8PL-Z7s' target='_blank' rel='noopener'>Ver video</<a>`,`<iframe width="560" height="315" src="https://www.youtube.com/embed/JSwu8PL-Z7s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`)
+    responsiveMedia('gmaps','(min-width: 1024px)',`<a href='https://www.google.com/maps/@6.2649263,-75.5776751,14.5z' target='_blank' rel='noopener'>Ver mapa</<a>`,`<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31727.927191164243!2d-75.5776751!3d6.2649263!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e44291049b3e057%3A0xe2496319a7e30efa!2sTiendas%20D1%20El%20Diamante!5e0!3m2!1ses-419!2sco!4v1683075788103!5m2!1ses-419!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`)
 })
 
 d.addEventListener('keydown', e => {
     // shortcuts(e)
     moveBall(e,'.ball','.stage')
 })
+
+tema('.btnTema','modo')
+
---
+++ temas.js
@@ -0,0 +1,33 @@
+const d = document
+let sun = '☀️', moon = '🌒'
+
+
+export default function tema(boton, tema) {
+  
+  const $temaBtn = d.querySelector(boton),
+    $selectors = d.querySelectorAll('[data-modo]')
+
+  const darkMode = () => {
+    $selectors.forEach((el) => el.classList.add(tema))
+    $temaBtn.textContent = sun
+    localStorage.setItem('tema', 'dark')
+  }
+
+  const lightMode = () => {
+    $selectors.forEach((el) => el.classList.remove(tema))
+    $temaBtn.textContent = moon
+    localStorage.setItem('tema', 'light')
+  }
+
+  d.addEventListener('click', e => {
+    
+    if (e.target.matches(boton)) {
+      if ($temaBtn.textContent === moon) { darkMode() } else lightMode()
+    }
+  })
+  d.addEventListener('DOMContentLoaded', e => {
+    if (localStorage.getItem('tema') === null) localStorage.setItem('tema', 'light')
+    if (localStorage.getItem('tema') === 'light') lightMode()
+    if (localStorage.getItem('tema') === 'dark') darkMode()
+  })
+}
