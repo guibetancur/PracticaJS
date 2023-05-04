fd3e6001ad88c6d2b0c3c833942a382876414861
Guillermo Betancur <guibetancur@gmail.com>
2023-05-03 19:28:58 -0500

    Nuevas secciones

---
+++ responsive_tester.js
@@ -0,0 +1,9 @@
+const d= document
+export default function responsiveTester(form) {
+  const $form = d.getElementById(form)
+  let tester // Variable para almacenar la ventana que se va a abrir y a cerrar
+
+  d.addEventListener('submit', e => {
+    
+  })
+}
--- retodom.js
+++ retodom.js
@@ -5,6 +5,7 @@ import countdown from "./section3.js";
 import botonHome from "./botonhome.js";
 import tema from "./temas.js";
 import responsiveMedia from "./responsive.js";
+import responsiveTester from "./responsive_tester.js";
 
 const d = document
 
@@ -17,6 +18,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
     botonHome('.scroll-top-btn')
     responsiveMedia('youtube','(min-width: 1024px)',`<a href='https://youtu.be/JSwu8PL-Z7s' target='_blank' rel='noopener'>Ver video</<a>`,`<iframe width="560" height="315" src="https://www.youtube.com/embed/JSwu8PL-Z7s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`)
     responsiveMedia('gmaps','(min-width: 1024px)',`<a href='https://www.google.com/maps/@6.2649263,-75.5776751,14.5z' target='_blank' rel='noopener'>Ver mapa</<a>`,`<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31727.927191164243!2d-75.5776751!3d6.2649263!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e44291049b3e057%3A0xe2496319a7e30efa!2sTiendas%20D1%20El%20Diamante!5e0!3m2!1ses-419!2sco!4v1683075788103!5m2!1ses-419!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`)
+    responsiveTester('responsiveTester')
 })
 
 d.addEventListener('keydown', e => {
